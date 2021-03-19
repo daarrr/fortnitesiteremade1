706a63da2fa1d7cb819b8cd67ad636fc420ecefd
daarrr <80875893+daarrr@users.noreply.github.com>
2021-03-19 05:23:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Fortnite site remade has been Made by daar , TikTok @daar , no this is not skidded or anything this is the fortnite site remade by daar with my own fonts styles ect, and html and css by me and other programs, and this is not affiliated with epicgames">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Fortnite Site Remade By Daar</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Home.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.9.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Luckiest+Guy:400">
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "index.html"
+}</script>
+    <meta property="og:title" content="Fortnite Site Remade By Daar">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+  </head>
+  <body data-home-page="Home.html" data-home-page-title="Fortnite Site Remade By Daar" class="u-body"><header class="u-clearfix u-grey-80 u-header u-header" id="sec-bdf7"><nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+        <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+          <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+            <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+          </a>
+        </div>
+        <div class="u-custom-menu u-nav-container">
+          <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="https://discord.gg/UBAff5enhN" target="_blank" style="padding: 10px 42px;">Fortnite Site Remade By Daar</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="https://discord.gg/UBAff5enhN" target="_blank" style="padding: 10px 42px;">Signed In As: Fortnite SiteRemade</a>
+</li></ul>
+        </div>
+        <div class="u-custom-menu u-nav-container-collapse">
+          <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+            <div class="u-sidenav-overflow">
+              <div class="u-menu-close"></div>
+              <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://discord.gg/UBAff5enhN" target="_blank" style="padding: 10px 32px;">Fortnite Site Remade By Daar</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="https://discord.gg/UBAff5enhN" target="_blank" style="padding: 10px 32px;">Signed In As: Fortnite SiteRemade</a>
+</li></ul>
+            </div>
+          </div>
+          <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+        </div>
+      </nav><a href="https://discord.gg/UBAff5enhN" class="u-btn u-button-style u-hover-palette-3-light-1 u-palette-3-base u-btn-1" target="_blank">DISCORD</a><h3 class="u-custom-font u-text u-text-1">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-white u-btn-2" href="https://discord.gg/UBAff5enhN" target="_blank">Fortnite Site Remade</a>
+      </h3></header>
+    <section class="u-clearfix u-image u-section-1" id="sec-6ac8" data-image-width="1920" data-image-height="1080">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://discord.gg/UBAff5enhN" class="u-btn u-btn-round u-button-style u-gradient u-none u-radius-9 u-text-black u-text-hover-grey-80 u-btn-1" data-animation-name="flipIn" data-animation-duration="1000" data-animation-delay="0" data-animation-direction="X" target="_blank">JOIN DISCORD</a>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-grey-10 u-section-2" src="" id="sec-50f4">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-gallery u-layout-grid u-lightbox u-show-text-on-hover u-gallery-1">
+          <div class="u-gallery-inner u-gallery-inner-1">
+            <div class="u-effect-fade u-effect-hover-liftUp u-gallery-item u-radius-11 u-shape-round u-gallery-item-1">
+              <div class="u-back-slide" data-image-width="1155" data-image-height="770">
+                <img class="u-back-image u-expanded" src="images/fortnite-chapter-2-season-6-03.jpg">
+              </div>
+              <div class="u-over-slide u-shading u-over-slide-1">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+            <div class="u-effect-fade u-effect-hover-liftUp u-gallery-item u-radius-11 u-shape-round u-gallery-item-2">
+              <div class="u-back-slide" data-image-width="1280" data-image-height="720">
+                <img class="u-back-image u-expanded" src="images/Fortnite-Chapter-2-Season-6-Wolves.jpg">
+              </div>
+              <div class="u-over-slide u-shading u-over-slide-2">
+                <h3 class="u-gallery-heading"></h3>
+                <p class="u-gallery-text"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="u-container-style u-group u-opacity u-opacity-75 u-palette-1-dark-3 u-radius-10 u-shape-round u-group-1" data-animation-name="lightSpeedIn" data-animation-duration="1000" data-animation-delay="0" data-animation-direction="">
+          <div class="u-container-layout u-container-layout-1">
+            <h2 class="u-subtitle u-text u-text-1">
+              <span style="font-size: 1.25rem;">Fortnite site remade has been Made by daar , TikTok @daar , no this is not skidded or anything this is the fortnite site remade by daar with my own fonts styles ect, and html and css by me and other programs, and this is not affiliated with epicgames</span>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-ac8e"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">This Is NOT Associated With Epic Games, Inc. in the United States of America and elsewhere., This Is ust The Fortnite Site Remade By Our Own HTML CSS ect © 2021 All rights reserved.<br>
+          <br>By Daar&nbsp;&nbsp;
+        </p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+    </section><span style="height: 64px; width: 64px; margin-left: 0px; margin-right: auto; margin-top: 0px; background-image: none; right: 20px; bottom: 20px;" class="u-back-to-top u-icon u-icon-circle u-opacity u-opacity-80 u-palette-3-base u-spacing-13" data-href="#">
+        <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 551.13 551.13"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-1d98"></use></svg>
+        <svg class="u-svg-content" enable-background="new 0 0 551.13 551.13" viewBox="0 0 551.13 551.13" xmlns="http://www.w3.org/2000/svg" id="svg-1d98"><path d="m275.565 189.451 223.897 223.897h51.668l-275.565-275.565-275.565 275.565h51.668z"></path></svg>
+    </span>
+  </body>
+</html>
